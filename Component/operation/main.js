0dc07a2543bcb4420ebb66220b2b3fc39bdc5ef8
Alexander Hernández <ja.hernandez211@gmail.com>
2023-07-03 22:58:01 -0500

    function sumar

--- Component/operation/main.js
+++ Component/operation/main.js
@@ -2,7 +2,7 @@ var montoUser = 1000000;
 var montoUser2 = 2000000;
 var montoUser3 = 3000000;
 
-var newMontoUser = 0;
+
 // const button =document.getElementById("button");
 // const container_modal =document.getElementById("container_modal");
 // const mod_btn = document.getElementById("mod_btn");
@@ -11,40 +11,65 @@ function consultarSaldo(){
     localStorage.setItem("action", "consultar");     //se asignar la variable consultar
     this.visible();
     if (localStorage.getItem("user") == "alexander"){   //me llama la variable y le asigna otra
-        if(newMontoUser != 0){
-            document.getElementById('texto_nav1').innerHTML = newMontoUser; //para pintar variable en html
-        }else{
-            document.getElementById('texto_nav1').innerHTML = montoUser;            
-        }
+        document.getElementById('monto').innerHTML = montoUser;            
         document.getElementById('user').innerHTML = "Alexander";   //pintar variable en html
+        
     }else if (localStorage.getItem("user") == "johan"){
-        document.getElementById('texto_nav1').innerHTML = montoUser2;
+        document.getElementById('monto').innerHTML = montoUser2;
         document.getElementById('user').innerHTML = "Johan";
     }else{
-        document.getElementById('texto_nav1').innerHTML = montoUser3;
+        document.getElementById('monto').innerHTML = montoUser3;
         document.getElementById('user').innerHTML = "Viviana";
     }
 }
+
+
 
 function getInto(){
     localStorage.setItem("action", "ingresar");
     this.visible();
 }
-function getIntoOperation(){
-    let inputValue = document.getElementById("num").value;
 
-    if (localStorage.getItem("user") == "alexander"){
-        this.newMontoUser =  parseInt(inputValue) + parseInt(montoUser);
-        this.consultarSaldo();
+let boton = document.getElementById("sumar");
+let newMonto = document.getElementById("monto");
 
-    }else if (localStorage.getItem("user") == "johan"){
-        this.newMontoUser =parseInt(inputValue)+parseInt(montoUser2);
-        this.consultarSaldo();
-    }else{
-        this.newMontoUser =parseInt(inputValue)+parseInt(montoUser3);
-        this.consultarSaldo();
-    }
+newMonto.addEventListener('click', hacerSuma());
+
+function hacerSuma(){
+    let num1 = parseFloat(document.getElementById('montoUser').value); 
+    let num = parseFloat(document.getElementById('num').value); 
+    let suma = num1 + num;
+    newMonto.innerHTML = `Su monto es de ${suma}`;
 }
+
+// function sumar()
+// {
+//   const $total = document.getElementById('montoUser');
+//   let subtotal = 0;
+//   [ ...document.getElementsByClassName( "monto" ) ].forEach( function ( number ) {
+//     if(number.value !== '') {
+//       subtotal += parseFloat(number.value);
+//     }
+//   });
+//   $total.value = subtotal;
+// }
+
+
+// function getIntoOperation(){
+//     let inputValue = document.getElementById("num").value;
+
+//     if (localStorage.getItem("user") == "alexander"){
+//         this.newMontoUser =  parseInt(inputValue) + parseInt(montoUser);
+//         this.consultarSaldo();
+
+//     }else if (localStorage.getItem("user") == "johan"){
+//         this.newMontoUser =parseInt(inputValue)+parseInt(montoUser2);
+//         this.consultarSaldo();
+//     }else{
+//         this.newMontoUser =parseInt(inputValue)+parseInt(montoUser3);
+//         this.consultarSaldo();
+//     }
+// }
 
 function withdraw(){
     localStorage.setItem("action", "retirar");
